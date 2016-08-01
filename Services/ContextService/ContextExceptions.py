eb910e1b0855e289ff03f9b43f9c220dab39623d
Renato Samperio <renatosamperio@gmail.com>
2016-08-01 22:40:49 +0200

    ContextException (#54): Adding a string representation to context exception

--- Services/ContextService/ContextExceptions.py
+++ Services/ContextService/ContextExceptions.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
 class ContextError(RuntimeError):
-   def __init__(self, arg, name):
-    self.args = arg
-    self.name = name
+    def __init__(self, arg, name):
+      self.args = arg
+      self.name = name
+    
+    def __str__(self):
+      msg = ''.join(self.args)
+      suffix=''.join(self.name)
+      return "<< "+str(msg)+":"+str(suffix)+" >>"
