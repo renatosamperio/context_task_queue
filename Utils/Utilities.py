f31ba856743906520fb5d72467b1b3affc6cf13b
Renato Samperio <renatosamperio@gmail.com>
2016-09-03 22:44:59 +0200

    Utilities: Adding method to print human readable quantities of bytes

--- Utils/Utilities.py
+++ Utils/Utilities.py
@@ -16,6 +16,14 @@ LOG_FILENAME	= 'context_provider.log'
 LOG_NAME	= 'ContextProvider'
 
   
+def GetHumanReadable(size,precision=2):
+    suffixes=['B','KB','MB','GB','TB']
+    suffixIndex = 0
+    while size > 1024 and suffixIndex < 4:
+        suffixIndex += 1 #increment the index of the suffix
+        size = size/1024.0 #apply the division
+    return "%.*f%s"%(precision,size,suffixes[suffixIndex])
+  
 def MemoryUsage(pid, log=None):
   '''Returns the memory usage in MB'''
   start = time.time()
