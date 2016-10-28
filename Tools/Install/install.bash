43c463f6426554afb58aaf5ef1090f80dc21496d
Renato Samperio <renatosamperio@gmail.com>
2016-10-28 23:14:50 +0200

    Install.Bash (#11): Created an installation bash script for installing dependencies, downloading required files and setting up working conditions

---
+++ Tools/Install/install.bash
@@ -0,0 +1,131 @@
+#!/bin/bash   
+
+PathScripts=$(echo $0 | awk -F'ins' '{print $1}')
+
+echo -e "\e[92m \e[1m"
+echo "*****************************************************"
+echo "*****          INSTALLING DEPENDENCIES          *****"
+echo "*****       (Python, Git, LibXML and ZMQ)       *****"
+echo "*****************************************************"
+echo -e "\e[0m"
+apt-get --assume-yes install python-setuptools python-dev build-essential python-pip python-pycurl librtmp-dev git libxml2-dev libxslt1-dev libzmq-dev libzmq5 python-zmq
+
+echo -e "\e[92m \e[1m"
+echo "*****************************************************"
+echo "*****          INSTALLING DEPENDENCIES          *****"
+echo "*****      (LibCurl OpenSSL and GNU utils)      *****"
+echo "*****************************************************"
+echo -e "\e[0m"
+apt-get --assume-yes install libcurl4-openssl-dev 
+apt-get --assume-yes install libcurl4-gnutls-dev
+
+echo -e "\e[92m \e[1m"
+echo "*****************************************************"
+echo "*****          INSTALLING DEPENDENCIES          *****"
+echo "*****                 (Tshark)                  *****"
+echo "*****************************************************"
+echo -e "\e[0m"
+## Tshark requires pressing button in installation
+apt-get --assume-yes install tshark 
+
+echo -e "\e[92m \e[1m"
+echo "*****************************************************"
+echo "*****          INSTALLING DEPENDENCIES          *****"
+echo "***** (Pip, GoogleAPI, PyVirtual and Selenium)  *****"
+echo "*****************************************************"
+echo -e "\e[0m"
+pip install --upgrade pip
+pip install --upgrade google-api-python-client
+pip install --upgrade pyvirtualdisplay
+# pip install --upgrade selenium
+
+
+## Installing GIT repositories
+
+echo -e "\e[92m \e[1m"
+echo "*****************************************************"
+echo "*****                                           *****"
+echo "*****      RESETTING PREVIOUS INSTALLATION      *****"
+echo "*****                                           *****"
+echo "*****************************************************"
+echo -e "\e[0m"
+rm -rf ~/workspace/setup
+mkdir -p ~/workspace/setup && cd ~/workspace/setup
+
+echo -e "\e[92m \e[1m"
+echo "*****************************************************"
+echo "*****          INSTALLING DEPENDENCIES          *****"
+echo "*****               (GIT: PsUtil)               *****"
+echo "*****************************************************"
+echo -e "\e[0m"
+git clone https://github.com/giampaolo/psutil.git && cd psutil/
+python setup.py install && cd ..
+
+echo -e "\e[92m \e[1m"
+echo "*****************************************************"
+echo "*****          INSTALLING DEPENDENCIES          *****"
+echo "*****              (GIT: PyShark)               *****"
+echo "*****************************************************"
+echo -e "\e[0m"
+git clone https://github.com/KimiNewt/pyshark.git && cd pyshark/src/
+git apply --stat ${PATH_FIX}/fix_raw_xml_da077b5b6ec19a6b0b8e13045cecb6d741d9a0c3.patch
+python setup.py install && cd ../..
+
+echo -e "\e[92m \e[1m"
+echo "*****************************************************"
+echo "*****          INSTALLING DEPENDENCIES          *****"
+echo "*****              (GIT: XMLDict)               *****"
+echo "*****************************************************"
+echo -e "\e[0m"
+git clone https://github.com/martinblech/xmltodict.git && cd xmltodict/
+python setup.py install && cd ..
+
+echo -e "\e[92m \e[1m"
+echo "*****************************************************"
+echo "*****          INSTALLING DEPENDENCIES          *****"
+echo "*****            (GIT: OAuthClient)             *****"
+echo "*****************************************************"
+echo -e "\e[0m"
+git clone https://github.com/google/oauth2client.git && cd oauth2client
+python setup.py install && cd ..
+
+echo -e "\e[92m \e[1m"
+echo "*****************************************************"
+echo "*****          INSTALLING DEPENDENCIES          *****"
+echo "*****             (GIT: GeckoDriver)            *****"
+echo "*****************************************************"
+echo -e "\e[0m"
+wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz
+tar -zxvf geckodriver-v0.11.1-linux64.tar.gz
+mkdir -p /opt/geckodriver
+sudo -u $USER echo 'export PATH=$PATH:/opt/geckodriver'  >> ~/.bashrc
+
+echo -e "\e[92m \e[1m"
+echo "*****************************************************"
+echo "*****          INSTALLING DEPENDENCIES          *****"
+echo "*****              (GIT: MongoDB)               *****"
+echo "*****************************************************"
+echo -e "\e[0m"
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-get update
+apt-get install -y mongodb-org
+wget https://raw.githubusercontent.com/renatosamperio/context_task_queue/master/Tools/Templates/Context.tmpl
+if [ ! -f /tmp/foo.txt ]; then
+    echo -e "\e[92m \e[1m"
+    echo "*****************************************************"
+    echo "*****          Found Mongo System File          *****"
+    echo "*****************************************************"    
+    echo -e "\e[0m"
+    cp mongodb.service /etc/systemd/system/
+    systemctl start mongodb
+fi
+
+echo -e "\e[92m \e[1m"
+echo "*****************************************************"
+echo "*****          INSTALLING DEPENDENCIES          *****"
+echo "*****      (GIT: Python driver for MongoDB)     *****"
+echo "*****************************************************"
+echo -e "\e[0m"
+git clone https://github.com/mongodb/mongo-python-driver.git && cd mongo-python-driver/
+python setup.py install && cd ..
