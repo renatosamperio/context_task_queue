99b3b6a3211ed8f8e8148623c58d2ede984e6724
Renato Samperio <renatosamperio@gmail.com>
2016-11-17 22:30:09 +0100

    ModuleLoader (#87): Enhancing lading module method  - Returns system installed class from task service type  - Validates search path  - Change log format

--- Utils/ModuleLoader.py
+++ Utils/ModuleLoader.py
@@ -28,9 +28,17 @@ class ModuleLoader:
     To get an instance is required to:
       1) Know the name of loaded class inside the found python module 
       2) Expect the loaded class has the same name as the found python module
+      ## TODO: This method has too many if's
     '''
     try:
-      self.logger.debug("  + Getting instance in [%s]" % (path))
+      self.logger.debug("     Getting instance in [%s]" % (path))
+      
+      ## If it is system module do not recompile class
+      recompile = True
+      if searchPath is None:
+	recompile = False
+	
+      ## Start splitting module dotted name in parts
       path_parts= path.split('.')
       sModules  = len(path_parts)
       lastPackage =''
@@ -38,42 +46,65 @@ class ModuleLoader:
 	moduleName = path_parts[i]
 	
 	## Preparing file for method arguments
-	if searchPath is not None and type(searchPath) is not list:
-	  searchPath = [searchPath]
-	searchPath = [searchPath[0]+"/"+lastPackage]
-	
+	## TODO: Is there a better way of doing it?
+	if searchPath is not None:
+	  if type(searchPath) is not list:
+	      searchPath = [searchPath]
+	  if len(searchPath)>0:
+	    fSlash = '' if searchPath[0].endswith('/') else '/'
+	    searchPath = [searchPath[0]+fSlash+lastPackage]
+
 	## Getting information from given path
 	f, fileName, description = imp.find_module(moduleName, searchPath)
 	importType = self.module_types[description[2]]
-	self.logger.debug("  + Loading [%s] of type [%s]" % ( moduleName, importType))
+	self.logger.debug("     Loading [%s] of type [%s]" % ( moduleName, importType))
 	
 	## Loading module
 	loadingObject = imp.load_module(moduleName, f, fileName, description)
 	moduleType = self.module_types[description[2]]
-	self.logger.debug("    [%s] is a [%s]"%(moduleName, moduleType))
+	self.logger.debug("     [%s] is a [%s]"%(moduleName, moduleType))
 	
 	## Concatenating last package in the search directory as we may
 	### find the class inside it
 	if moduleType == 'package':
-	  lastPackage = moduleName
 	  
-        # If the module is source, get the class, 
-        #    will raise AttributeError if class cannot be found
-	if moduleType == 'source':
+	  ## This is not smart at all, the end is removed and added later to
+	  ## keep consitency with non-installed modules
+	  if fileName.endswith(moduleName):
+	    removeSpaces = -1 * len(moduleName)
+	    fileName = fileName[:removeSpaces]
+	    
+	  ## Not happy at all why this is happening?
+	  lastPackage = moduleName
+	  searchPath = fileName
+	  
+	# If the module is source, get the class, 
+	#    will raise AttributeError if class cannot be found
+	elif moduleType == 'source':
+	  
+	  ## If it was a system module, all is done...
+	  if not recompile:
+	    self.logger.debug("     Found system module [%s]"%path)
+	    
+	    ## Class is somewhere inside the modules
+	    if moduleName in loadingObject.__dict__.keys():
+	      classObject = loadingObject.__dict__[moduleName]
+	      return classObject
+	  
 	  ## Recompiling module
 	  recompiledClass = moduleName+description[0]
-	  self.logger.debug("    Re-compiling class [%s]"%(recompiledClass))
+	  self.logger.debug("     Re-compiling class [%s]"%(recompiledClass))
 	  py_compile.compile(fileName)
 	  
 	  ## Reloading class
 	  reloadedClass = fileName+"c"
-	  self.logger.debug("    Re-loading class [%s]"%(recompiledClass))
+	  self.logger.debug("     Re-loading class [%s]"%(recompiledClass))
 	  newLoaded = imp.load_compiled(moduleName, reloadedClass)
 	  
 	  ## Reloading module in case code has been updated
 	  for m in sys.modules:
 	    if moduleName in m and '.' in m:
-	      self.logger.debug("    Re-loading module [%s]"%(moduleName))
+	      self.logger.debug("     Re-loading module [%s]"%(moduleName))
 	      imp.reload(sys.modules[m])
 	  
 	  ## Choosing the class to load
@@ -87,13 +118,13 @@ class ModuleLoader:
 	  ##	   not the same name as the found module or it was 
 	  ##	   not input in the parameters of this method
 	  if loadedClass is not None:
-	    self.logger.debug("    Getting a class [%s]"%(loadedClass))
+	    self.logger.debug("     Getting a class [%s]"%(loadedClass))
 	    classObj = getattr(newLoaded, loadedClass)
 	    return classObj
 	  else:
 	    ## TODO: The class is somewhere in the new loaded module
 	    ## 	     and should be gotten
-	    self.logger.debug("    Class not found in module [%s]"%moduleName)
+	    self.logger.debug("     Class not found in module [%s]"%moduleName)
 	    return None
 	    
     except Exception as inst:
